4ecf6897e88e8e5caff3d9914b1364f9675d55a5
JensDiemer <git@jensdiemer.de>
2017-11-26 14:15:11 +0100

    try Kivy-Travis-CI-Template
    https://github.com/paulheider/Kivy-Travis-CI-Template

---
+++ travis/script_linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Linux specific Travis script
+
+# exits on fail
+set -e
+
+# creates & uses the virtualenv
+virtualenv --python=python2.7 --no-site-packages venv
+source venv/bin/activate
+
+# installs requirements
+pip install --install-option="--no-cython-compile" $(grep Cython requirements.txt)
+pip install -r tests/requirements.txt
+pip install -r requirements.txt
+
+# runs tests
+python -m pytest tests
