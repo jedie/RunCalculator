4ff201f103f6a8e88b0d322b1bd1d625e8da384a
JensDiemer <git@jensdiemer.de>
2017-11-26 14:59:46 +0100

    display output directly to travis

--- travis/buildozer_android.sh
+++ travis/buildozer_android.sh
@@ -3,20 +3,20 @@
 # exits on fail
 set -e
 
-source travis/tools.sh
+#source travis/tools.sh
 
 # start the ping loop
-ping_loop
+#ping_loop
 
 # start the buildozer build
 
 set -x
 cd runcalculator
-buildozer android debug >> $BUILD_OUTPUT 2>&1
-buildozer android release >> $BUILD_OUTPUT 2>&1
+buildozer android debug #>> $BUILD_OUTPUT 2>&1
+buildozer android release #>> $BUILD_OUTPUT 2>&1
 
 # the build finished without returning an error so dump a tail of the output
-dump_output
+#dump_output
 
 # nicely terminate the ping output loop
-kill $PING_LOOP_PID
+#kill $PING_LOOP_PID
