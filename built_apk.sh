c158064b8bf526a220c4e4773e03131b0f8e888d
JensDiemer <git@jensdiemer.de>
2017-11-28 19:22:31 +0100

    work-a-round for travis, see:
    https://travis-ci.org/jedie/RunCalculator/builds/308567084#L515

--- built_apk.sh
+++ built_apk.sh
@@ -7,9 +7,24 @@
 #   ./built_apk.sh --verbose
 #
 
-CHANGE_USER=-v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro -u=$UID:$(id -g $USER)
-
 set -e
+
+# Generate passwd/group file, e.g.: doesn't exist on travis ;)
+
+TEMP=$(python -c "import tempfile;print(tempfile.gettempdir())")
+
+TEMP_PASSWD=${TEMP}/passwd
+TEMP_GROUP=${TEMP}/group
+
+DOCKER_UID=$(id -u)
+DOCKER_UGID=$(id -g)
+DOCKER_USER=${USER}
+
+echo "${DOCKER_USER}:x:${DOCKER_UID}:${DOCKER_UGID}:${DOCKER_USER},,,:/home/${DOCKER_USER}:/bin/bash">${TEMP_PASSWD}
+echo "${DOCKER_USER}:x:${DOCKER_UGID}:">${TEMP_GROUP}
+
+CHANGE_USER="-v ${TEMP_GROUP}:/etc/group:ro -v ${TEMP_PASSWD}:/etc/passwd:ro -u=$UID:$(id -g $USER)"
+
 set -x
 
 docker pull jedie/buildozer
@@ -21,6 +36,9 @@ docker pull jedie/buildozer
     docker run --tty --volume ${PWD}:/buildozer/ jedie/buildozer pip freeze
     docker run --tty --volume ${PWD}:/buildozer/ jedie/buildozer cython --version
     docker run --tty --volume ${PWD}:/buildozer/ jedie/buildozer buildozer --version
+
+#    docker run --tty --volume ${PWD}:/buildozer/ ${CHANGE_USER} jedie/buildozer id
+#    docker run --tty --volume ${PWD}:/buildozer/ ${CHANGE_USER} jedie/buildozer ls -la
 
     docker run --tty --volume ${PWD}:/buildozer/ ${CHANGE_USER} jedie/buildozer buildozer ${1} android debug
     docker run --tty --volume ${PWD}:/buildozer/ ${CHANGE_USER} jedie/buildozer buildozer ${1} android release
