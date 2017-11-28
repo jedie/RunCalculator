5b6301c40daf506a4ababadea60f3b4d51712a59
JensDiemer <git@jensdiemer.de>
2017-11-28 19:10:24 +0100

    work-a-round for permission errors

--- built_apk.sh
+++ built_apk.sh
@@ -7,6 +7,8 @@
 #   ./built_apk.sh --verbose
 #
 
+CHANGE_USER=-v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro -u=$UID:$(id -g $USER)
+
 set -e
 set -x
 
@@ -20,10 +22,8 @@ docker pull jedie/buildozer
     docker run --tty --volume ${PWD}:/buildozer/ jedie/buildozer cython --version
     docker run --tty --volume ${PWD}:/buildozer/ jedie/buildozer buildozer --version
 
-    docker run --tty --volume ${PWD}:/buildozer/ jedie/buildozer ls -Rla
-
-    docker run --tty --volume ${PWD}:/buildozer/ jedie/buildozer buildozer ${1} android debug
-    docker run --tty --volume ${PWD}:/buildozer/ jedie/buildozer buildozer ${1} android release
+    docker run --tty --volume ${PWD}:/buildozer/ ${CHANGE_USER} jedie/buildozer buildozer ${1} android debug
+    docker run --tty --volume ${PWD}:/buildozer/ ${CHANGE_USER} jedie/buildozer buildozer ${1} android release
 )
 
 ls -la runcalculator/bin/
