f3c47f0249757f74140780e270d54e50b6c98379
JensDiemer <git@jensdiemer.de>
2017-11-28 19:31:51 +0100

    fixup! travis

--- built_apk.sh
+++ built_apk.sh
@@ -20,7 +20,7 @@ DOCKER_UID=$(id -u)
 DOCKER_UGID=$(id -g)
 DOCKER_USER=${USER}
 
-echo "${DOCKER_USER}:x:${DOCKER_UID}:${DOCKER_UGID}:${DOCKER_USER},,,:/home/${DOCKER_USER}:/bin/bash">${TEMP_PASSWD}
+echo "${DOCKER_USER}:x:${DOCKER_UID}:${DOCKER_UGID}:${DOCKER_USER},,,:/buildozer/:/bin/bash">${TEMP_PASSWD}
 echo "${DOCKER_USER}:x:${DOCKER_UGID}:">${TEMP_GROUP}
 
 CHANGE_USER="-v ${TEMP_GROUP}:/etc/group:ro -v ${TEMP_PASSWD}:/etc/passwd:ro -u=$UID:$(id -g $USER)"
@@ -37,8 +37,9 @@ docker pull jedie/buildozer
     docker run --tty --volume ${PWD}:/buildozer/ jedie/buildozer cython --version
     docker run --tty --volume ${PWD}:/buildozer/ jedie/buildozer buildozer --version
 
-#    docker run --tty --volume ${PWD}:/buildozer/ ${CHANGE_USER} jedie/buildozer id
-#    docker run --tty --volume ${PWD}:/buildozer/ ${CHANGE_USER} jedie/buildozer ls -la
+    docker run --tty --volume ${PWD}:/buildozer/ ${CHANGE_USER} jedie/buildozer id
+    docker run --tty --volume ${PWD}:/buildozer/ ${CHANGE_USER} jedie/buildozer pwd
+    docker run --tty --volume ${PWD}:/buildozer/ ${CHANGE_USER} jedie/buildozer ls -la
 
     docker run --tty --volume ${PWD}:/buildozer/ ${CHANGE_USER} jedie/buildozer buildozer ${1} android debug
     docker run --tty --volume ${PWD}:/buildozer/ ${CHANGE_USER} jedie/buildozer buildozer ${1} android release
